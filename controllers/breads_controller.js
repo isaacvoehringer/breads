e0aefda7669fb3691497ab24cf71827869a6f80b
isaac <voehringer.isaac@gmail.com>
2022-09-27 20:16:55 -0500

    sending index

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -5,7 +5,12 @@ const Bread = require('../models/bread.js')
 //index
 
 breads.get('/', (req,res) => {
-    res.send(Bread)
+    res.render('index',
+    {
+        breads: Bread
+      }
+      )
+    //res.send(Bread)
 })
 
 // SHOW
